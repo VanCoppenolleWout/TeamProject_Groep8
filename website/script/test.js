3a594a65e88f587512b8048de5f35ff8ded86e91
Wout Van Coppenolle <woutvancoppenolle6@gmail.com>
2021-01-12 12:00:45 +0100

    gitflow-feature-stash: leaderboard
    data is in leaderboard

--- website/script/test.js
+++ website/script/test.js
@@ -1,27 +1,55 @@
-const toggleState = function() {
-   var d1 = document.getElementById("js-geslotendiv");
-   var d2 = document.getElementById("js-opendiv");
-   if( d2.style.display == "none" )
-   {
-      d1.style.display = "none";
-      d2.style.display = "flex";
-   }
-   else
-   {
-      d1.style.display = "flex";
-      d2.style.display = "none";
-   }
+let showResult = (queryResponse) => {
+    // log all objects
+    for (const element of queryResponse) {
+        console.log(element);
+      }
+
+    // Top 3 objects
+    var nr1 = queryResponse[0];
+    var nr2 = queryResponse[1];
+    var nr3 = queryResponse[2];
+    const top3 = document.querySelector(".o-layout__top3");
+    top3.innerHTML = `<div class="o-layout__individueel">
+                                <p class="c-card__name">${nr2.playername}</p>
+                                <p class="c-card__score">${nr2.score}</p>
+                                <div class="c-card__podium2"><p class="c-position">2</p></div>
+                            </div>
+                            <div class="o-layout__individueel">
+                                <p class="c-card__name">${nr1.playername}</p>
+                                <p class="c-card__score">${nr1.score}</p>
+                                <div class="c-card__podium1"><p class="c-position">1</p></div>
+                            </div>
+                            <div class="o-layout__individueel">
+                                <p class="c-card__name">${nr3.playername}</p>
+                                <p class="c-card__score">${nr3.score}</p>
+                                <div class="c-card__podium3"><p class="c-position">3</p></div>
+                            </div>`;
+
+    // List outside of top 3
+    const html_list = document.querySelector(".js-leaderboard");
+    var html_place = 3;
+    var arrLeaderboard = queryResponse.slice(3);
+
+    for (const element of arrLeaderboard) {
+        html_place = html_place + 1;
+        html_list.innerHTML += `<ul class="o-list o-layout__list">
+                            <div class="o-layout__list-item">
+                                <li class="c-list__position">${html_place}</li>
+                                <li class="c-list__name">${element.playername}</li>
+                            </div>
+                            <li class="c-list__score">${element.score}</li>
+                        </ul>`;
+      };
+};
+
+const getAPI = async () => {
+    const data = await fetch(`https://trappenspel-api.azurewebsites.net/api/leaderboard`)
+        .then((r) => r.json())
+        .catch((err) => console.error('An error occured', err));
+        showResult(data);
+    //console.log(data);
 };
 
 document.addEventListener('DOMContentLoaded', function() {
-    console.log("DOM loaded")
-    const btn_uitleg_gesloten = document.querySelector(".js-uitleg__gesloten");
-    btn_uitleg_gesloten.addEventListener('click', event => {
-        toggleState();
-    });
-
-    const btn_uitleg_open = document.querySelector(".js-uitleg__open");
-    btn_uitleg_open.addEventListener('click', event => {
-        toggleState();
-    });
+    getAPI();
 });
